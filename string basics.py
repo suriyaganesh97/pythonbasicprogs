25d650a1c340ea5444fe4f753a6c855b353b0275
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2020-04-05 21:39:56 +0530

    string

--- string basics.py
+++ string basics.py
@@ -1,3 +1,12 @@
 print(3*'suriya')#to repeat a string
 print('\nsuriya')#here \n is new line
 print(r'\nsuriya')#here it is a raw sting whatever inside gets printed
+print("'ssg'")#to print single use them inside double
+print('"ssg"')#to print double use them inside single
+name='suriya'
+print(name[0])
+print(name[0:])
+print(name[-1])
+print(name[2])
+print(name[2:])
+print(name[:-1])
