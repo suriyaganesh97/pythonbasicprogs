2a0a7e764d71b22e729bc7b7a9e809d4a0aefb89
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2020-05-01 19:37:42 +0530

    Create range.py

---
+++ range.py
@@ -0,0 +1,11 @@
+for i in range(5):   # from 0 to 5
+    print(i)
+
+for i in range(3,5):  #to print from 3 to 5
+    print(i)
+
+for i in range(1,10,2):  #increments using 2
+    print(i)
+
+for i in range(20,10,-2):  # for descending order decrement must be specified with neg value before
+    print(i)
