356efb3dc37ca0cff69e100b2e317a9d92ad9d0f
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2020-05-08 17:07:20 +0530

    commit

---
+++ pass.py
@@ -0,0 +1,6 @@
+list1=[2,31,56,32]
+for i in list1:
+    if(i%2==0):
+        pass    #pass is simply used here to fill the suite or block of code
+    else:
+        print(i)
--- primeno.py
+++ primeno.py
@@ -1,4 +1,5 @@
 num1=int(input('enter a no'))
+num2=num1
 i=2
 flag=0
 while(i<num1):
@@ -9,3 +10,15 @@ if(flag==0):
     print('the no is prime number')
 else:
     print('the no is not a prime number')
+
+
+print()
+print('more efficient way in below program')
+for j in range(2,num1):      #can reduce the iteartion to num1/2 or square root of num1
+    if(num1%j==0):
+        print('not a prime no')
+        break
+else:    #using for else condition here
+    print('the no is a prime no')
+
+
