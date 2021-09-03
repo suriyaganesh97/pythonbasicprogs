beb005f2874543c1d58aa21c4cae8d1d99c82b3c
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-09-03 14:42:37 +0530

    d13 commit

---
+++ d13-debugging/main.py
@@ -0,0 +1,44 @@
+# Describe Problem
+# def my_function():
+#   for i in range(1, 20):
+#     print(i)
+#     if i == 20:  # condition wont work as loop works till only 19 just print and see it changing range to 21 should make the code work
+#       print("You got it")
+# my_function()
+
+# # Reproduce the Bug
+# from random import randint
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(0, 5)  #randint(1,6) this was the prev code which was giving index error
+# #print(dice_num)
+# print(dice_imgs[dice_num])
+#
+# # Play Computer
+# year = int(input("What's your year of birth?"))
+# if year > 1980 and year < 1994:
+#   print("You are a millenial.")
+# elif year >= 1994:
+#   print("You are a Gen Z.")
+#
+# # Fix the Errors
+# age = int(input("How old are you?"))
+# if age > 18:
+#     print(f"You can drive at age {age}.")
+#
+# #Print is Your Friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page = int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
+#
+# #Use a Debugger
+def mutate(a_list):
+  b_list = []
+  for item in a_list:
+    new_item = item * 2
+    b_list.append(new_item)
+  print(b_list)
+
+mutate([1,2,3,5,8,13])
