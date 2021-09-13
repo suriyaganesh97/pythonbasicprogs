dddf9ea8d5754a3315b68fc647ae636506204e82
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-09-13 22:37:26 +0530

    d22

--- ArcadeGame/main.py
+++ ArcadeGame/main.py
@@ -2,6 +2,8 @@ from turtle import Turtle
 from turtle import Screen
 import time
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
 screen = Screen()
 screen.title("Ping Pong Game")
 screen.setup(width=800, height=600)
@@ -10,8 +12,8 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
-
-
+ball = Ball()
+scoreboard = Scoreboard()
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
@@ -20,8 +22,26 @@ screen.onkey(l_paddle.go_down, "s")
 
 is_game_on = True
 while is_game_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
 
+    #detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #detect collison with paddles
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() > -320:
+        ball.bounce_x()
+
+    #detect r paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+    # detect l paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 
 screen.exitonclick()
