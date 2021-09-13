406957dcae3cd174670059b61daad56e7961c2ad
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-09-13 19:58:17 +0530

    d22 commit

---
+++ ArcadeGame/main.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+from turtle import Screen
+import time
+from paddle import Paddle
+screen = Screen()
+screen.title("Ping Pong Game")
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+
+
+
+screen.exitonclick()
---
+++ ArcadeGame/paddle.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self,position):
+        super().__init__()
+        self.color("white")
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
+
+    def go_up(self):
+        new_y_cor = self.ycor() + 20
+        self.goto(self.xcor(), new_y_cor)
+
+    def go_down(self):
+        new_y_cor = self.ycor() - 20
+        self.goto(self.xcor(), new_y_cor)
+
+
+
+
+
+
