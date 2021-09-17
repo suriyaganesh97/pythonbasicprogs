6629957b8da7e6e3810e5489366f9cf5afb9699a
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-09-17 09:55:44 +0530

    us states game completed

--- US_states/us-states-game-start/us-states-game-start/main.py
+++ US_states/us-states-game-start/us-states-game-start/main.py
@@ -1,3 +1,4 @@
+from turtle import Turtle
 import turtle
 import pandas
 data = pandas.read_csv("50_states.csv")
@@ -7,10 +8,33 @@ image = "blank_states_img.gif"
 turtle.addshape(image)
 turtle.shape(image)
 correct_guess = 0
-answer_state = screen.textinput(title="state name", prompt="enter a state name:")
-new_data = data["state"].str.contains(answer_state)
-# if data["state"].str.contains(answer_state):
-#     correct_guess += 1
-print(new_data)
-
+turtle = Turtle()
+turtle.hideturtle()
+turtle.penup()
+state_list = data["state"].to_list()
+correct_guess_list = []
+is_game_on = True
+while is_game_on:
+    answer_state = screen.textinput(title=f"state names {correct_guess}/50", prompt=f"enter a state name:")
+    answer = answer_state.title()
+    for state in state_list:
+        if state == answer:
+            correct_guess += 1
+            state_data = data[data.state == answer]
+            x_cor = int(state_data.x)
+            y_cor = int(state_data.y)
+            turtle.goto(x_cor, y_cor)
+            turtle.write(answer)
+            correct_guess_list.append(answer)
+    if correct_guess == 50:
+        is_game_on = False
+    if answer == "Exit":
+        missing_states = []
+        for states in state_list:
+            if states not in correct_guess_list:
+                missing_states.append(states)
+        #print(missing_states)
+        is_game_on = False
+new_data_state = pandas.DataFrame(missing_states)
+new_data_state.to_csv("statesToLearn.csv")
 #screen.exitonclick()
