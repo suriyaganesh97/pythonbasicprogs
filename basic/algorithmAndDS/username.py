485497874e6b75ebef18f0988a8be2306a963fe0
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-09-20 19:16:11 +0530

    notes commit

---
+++ basic/algorithmAndDS/username.py
@@ -0,0 +1,7 @@
+str1 = "suriya@gmail.com"
+str2 = str.split("@")[0]
+print(str2)
+
+
+# the value in the bracket indicates the occurence for the @ symbol if you put two @ symbol and
+# and then give value as 1 in bracket it will output the values within the @ symbols
