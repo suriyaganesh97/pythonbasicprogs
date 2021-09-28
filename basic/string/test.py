81ec65584033ecd5cc237737588d2edd35f2947e
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-09-28 12:26:51 +0530

    config change

--- basic/string/test.py
+++ basic/string/test.py
@@ -1,51 +1,11 @@
-def mergeArrays(arr1, arr2, n1, n2):
-    arr3 = [None] * (n1 + n2)  #intializing the array with n1 + n2 number of elements
-    i = 0
-    j = 0
-    k = 0
+st = "aaabba"
+n = len(st)
+i = 0
 
-    # Traverse both array
-    while i < n1 and j < n2:
-
-        # Check if current element
-        # of first array is smaller
-        # than current element of
-        # second array. If yes,
-        # store first array element
-        # and increment first array
-        # index. Otherwise do same
-        # with second array
-        if arr1[i] < arr2[j]:
-            arr3[k] = arr1[i]
-            k = k + 1
-            i = i + 1
-        else:
-            arr3[k] = arr2[j]
-            k = k + 1
-            j = j + 1
-
-    # Store remaining elements
-    # of first array
-    while i < n1:
-        arr3[k] = arr1[i];
-        k = k + 1
-        i = i + 1
-
-    # Store remaining elements
-    # of second array
-    while j < n2:
-        arr3[k] = arr2[j];
-        k = k + 1
-        j = j + 1
-    print("Array after merging")
-    for i in range(n1 + n2):
-        print(str(arr3[i]), end=" ")
-
-
-# Driver code
-arr1 = [1, 3, 5, 7]
-n1 = len(arr1)
-
-arr2 = [2, 4, 6, 8]
-n2 = len(arr2)
-mergeArrays(arr1, arr2, n1, n2);
+while i<n:
+    count =1
+    while i<n-1 and st[i]==st[i+1]:
+        count+=1
+        i+=1
+    i+=1
+    print(st[i-1]+str(count),end="")
