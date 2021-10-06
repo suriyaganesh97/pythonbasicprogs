5f805b585558da05d2c1ed2fdddeb4897eb95a57
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-10-06 21:49:18 +0530

    small changes

--- basic/algorithmAndDS/test.py
+++ basic/algorithmAndDS/test.py
@@ -1,2 +1,3 @@
-for i in range(1,21):
-    print(i)
+arr1 = [12,45,32,16,59,78,26]
+arr1.sort()
+print(arr1)
--- basic/pattern/pattern1.py
+++ basic/pattern/pattern1.py
@@ -3,7 +3,7 @@ i=1
 while(i<=4):
     j=1
     while(j<=4):
-        print('*',end="")   #by using this tend the execution continues in the same line and does not go to new line
+        print('*',end="")   #by using this end the execution continues in the same line and does not go to new line
         j=j+1
     print()
     i=i+1
