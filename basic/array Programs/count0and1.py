950df5dc17f801d75d0caaac7c7d3a3d3f22e184
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-10-11 10:16:33 +0530

    reverseofaString

---
+++ basic/array Programs/count0and1.py
@@ -0,0 +1,2 @@
+#Write a program to separate out 0 and 1 from array eg input [0,1,0,1] output [0,0, 1, 1]
+arr1 = [0,1,0,1]
---
+++ basic/string/ReverseofaString.py
@@ -0,0 +1,3 @@
+str1 = "suriya"
+str2 = str1[::-1] #using slicing concept
+print(str2)
