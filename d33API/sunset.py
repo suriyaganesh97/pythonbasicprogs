ab930979984e1017adec9e545c0b15c828b7ed50
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-10-17 17:31:13 +0530

    d33 final commit

---
+++ d33API/sunset.py
@@ -0,0 +1,12 @@
+# import requests
+#
+# my_lat=13.082680
+# my_lng=80.270721
+# parameters ={
+#     "lat": my_lat,
+#     "long": my_lng
+# }
+# response = requests.get(url="https://api.sunrise-sunset.org/json",params=parameters)
+# response.raise_for_status()
+
+#getting cert error any workaroung might cause secutiry issue so leave for now
