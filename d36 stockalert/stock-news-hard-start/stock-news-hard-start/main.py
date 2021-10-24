5fbd21ce9f4ed9d92885088c8a1ae34155c68a61
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-10-24 19:10:57 +0530

    d36 initial commit

--- d36 stockalert/stock-news-hard-start/stock-news-hard-start/main.py
+++ d36 stockalert/stock-news-hard-start/stock-news-hard-start/main.py
@@ -1,17 +1,35 @@
+import requests
+from datetime import datetime,timedelta
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+ALPHA_API_KEY = "B2ACEYZ0L9L10W8R"
+NEWS_API_KEY="eaaf681786bc4654b880a285b9604d3a"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+STOCK_PARAMETERS ={
+    "function":"TIME_SERIES_DAILY",
+    "symbol":STOCK,
+    "apikey":ALPHA_API_KEY
+}
 
-
-## STEP 1: Use https://newsapi.org/docs/endpoints/everything
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-#HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
-#HINT 2: Work out the value of 5% of yerstday's closing stock price. 
+#HINT 1: Get the closing price for yesterday and the day before yesterday.
+# Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
+#HINT 2: Work out the value of 5% of yerstday's closing stock price.
 
-
-
+# response  = requests.get(url=STOCK_ENDPOINT,params=STOCK_PARAMETERS)
+# response.raise_for_status()
+# yesterday_close_price = float(response.json()["Time Series (Daily)"]["2021-10-22"]["4. close"])
+# db_yesterday_close_price = float(response.json()["Time Series (Daily)"]["2021-10-21"]["4. close"])
+# #above values are hardcoded
+# if abs(yesterday_close_price - db_yesterday_close_price)>20:
+#     print("get news")
+# print(0.05*(yesterday_close_price))
+yesterday = datetime.now() - timedelta(2)
+yesterday_date = datetime.strftime(yesterday, '%Y-%m-%d')
+db_yesterday = datetime.now() - timedelta(2)
+db_yesterday_date = datetime.strftime(yesterday, '%Y-%m-%d')
 ## STEP 2: Use https://newsapi.org/docs/endpoints/everything
 # Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME. 
 #HINT 1: Think about using the Python Slice Operator
